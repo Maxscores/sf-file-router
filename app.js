1e95343e2951d3af8d8c53d4a0f3555bef2363db
maxscores <maxstackhouse@gmail.com>
2018-09-05 16:28:01 -0600

    body parser

--- app.js
+++ app.js
@@ -28,9 +28,9 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "*");
   res.header("Access-Control-Allow-Headers", "*");
-  req.header("Access-Control-Allow-Origin", "*");
-  req.header("Access-Control-Allow-Methods", "*");
-  req.header("Access-Control-Allow-Headers", "*");
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end()
+  }
   next();
 });
 
